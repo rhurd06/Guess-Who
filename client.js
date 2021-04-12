208fdb4587a1c3f8fc40989fd4c8f8cdd861c337
Robyn Noble <robynhurd09@gmail.com>
2021-04-12 16:25:36 -0500

    photos on the DOM

--- client.js
+++ client.js
@@ -1,1 +1,28 @@
 console.log('Here are all the available people:', people);
+
+$(document).ready(onReady);
+
+function onReady() {
+    addGithubUsers();
+    randomNumber();
+}//end onReady
+function addGithubUsers() {
+    //loop through people
+    for (let person of people){
+        $('#photoDivs').append(`
+       <div>
+       <img src="https://github.com/${person.githubUsername}.png?size=250"
+       alt="Profile image of ${person.name}"></img>
+       </div>
+       `);
+    }//end for loop
+}//end addPhotosToDom
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random( )*(1 + max-min) + min);
+}//end randomNumber
+
+
+
+
+
