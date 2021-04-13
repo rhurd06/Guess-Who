4b660eb35e601297333f6f87751ab860bc3e8943
Robyn Noble <robynhurd09@gmail.com>
2021-04-13 08:42:20 -0500

    added cohort

--- data.js
+++ data.js
@@ -27,4 +27,40 @@ var people = [
     name: 'Chris',
     githubUsername: 'christopher-black'
   },
+  {
+    name: 'Robyn',
+    githubUsername: 'rhurd06'
+  },
+  {
+    name: 'Reese',
+    githubUsername: 'Raveryk'
+  },
+  {
+    name: 'Maneena',
+    githubUsername: 'Manxiong26'
+  },
+  {
+    name: 'Karsten',
+    githubUsername: 'karstenjep'
+  },
+  {
+    name: 'Chloe',
+    githubUsername: 'chloeeverson'
+  },
+  {
+    name: 'Alex',
+    githubUsername: 'alexlloydwhite'
+  },
+  {
+    name: 'Eyram',
+    githubUsername: 'eyramtay'
+  },
+  {
+    name: 'Johnny',
+    githubUsername: 'Keplej'
+  },
+  {
+    name: 'John',
+    githubUsername: 'johnturner4004'
+  },
 ];
